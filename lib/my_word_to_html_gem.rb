a2e68c01433477be458007ec7d127b0e789b022d
Seif Eddine NOUARA <nouaraseifeddine@gmail.com>
2022-12-25 17:21:06 +0100

    fix code

--- lib/my_word_to_html_gem.rb
+++ lib/my_word_to_html_gem.rb
@@ -3,6 +3,24 @@
 require_relative "my_word_to_html_gem/version"
 
 module MyWordToHtmlGem
-  class Error < StandardError; end
-  # Your code goes here...
+  require "rubyzip"
+
+  class Converter
+    def initialize(file_path)
+      @file_path = file_path
+    end
+
+    def convert
+      zip_file = RubyZip::File.open(@file_path)
+      word_document = zip_file.find_entry("word/document.xml")
+
+      # Read the contents of the word document
+      doc_content = word_document.get_input_stream.read
+      # Replace all instances of "w:p" with "p" to convert to HTML
+      doc_content.gsub!("w:p", "p")
+
+      # Return the converted HTML
+      "<html><body>#{doc_content}</body></html>"
+    end
+  end
 end
