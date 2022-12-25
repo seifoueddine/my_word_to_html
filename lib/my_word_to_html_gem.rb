f8820cdd4555e877668a18ed2fe8ef1856cb6025
Seif Eddine NOUARA <nouaraseifeddine@gmail.com>
2022-12-25 16:57:20 +0100

    first commit

---
+++ lib/my_word_to_html_gem.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "my_word_to_html_gem/version"
+
+module MyWordToHtmlGem
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ word_to_html.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require "rubyzip"
+
+module WordToHTML
+  class Converter
+    def initialize(file_path)
+      @file_path = file_path
+    end
+
+    def convert
+      zip_file = RubyZip::File.open(@file_path)
+      word_document = zip_file.find_entry("word/document.xml")
+
+      # Read the contents of the word document
+      doc_content = word_document.get_input_stream.read
+      # Replace all instances of "w:p" with "p" to convert to HTML
+      doc_content.gsub!("w:p", "p")
+
+      # Return the converted HTML
+      "<html><body>#{doc_content}</body></html>"
+    end
+  end
+end
